42581743a8f6033575bed2ff71e08ecf78177160
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-08-28 13:49:32 +0000

    all generated customers seated

--- restaurant.rb
+++ restaurant.rb
@@ -7,19 +7,26 @@ class Restaurant
   include Enumerable
 
   def initialize
-  	@waiters   = []
-  	@customers = []
-  	@tables    = []
+    @all_seated_customers = []
+    @spare_tables         = []
+    @randoms              = []
+  	@waiters              = []
+  	@customers            = []
+  	@tables               = []
+    @count                = 0
   end
   
   def initiate_restaurant_activity
   	customers_enter_restaurant
   	customers_sit_and_wait
   	@tables.each do |table|
-  	  puts "table number: #{table.table_number} number of customers: #{table.seated_customers.size}"
+      if table.seated_customers.any?
+  	  puts "Table No.: #{table.table_number} number of customers: #{table.seated_customers.size}"
       table.seated_customers.each do |customer|
       	puts "customer id: #{customer.id}"
       end
+      end
+      puts ""
   	end
   	puts "#{@customers.size} customers"
   end
@@ -33,29 +40,67 @@ class Restaurant
   end
 
   def customers_sit_across_tables
-  	@randoms = []
   	@tables.each do |table|
   	  5.times do
-  	    @temp = randomize_customer    
-  	     table.seated_customers << @customer if !condition?(@customer)
+  	    randomize_customer 
+  	    table.seated_customers << @customer if !condition?(@customer) 
+        @all_seated_customers << @customer
   	  end
   	end
+
+    @array = remainder
+    catch_remainder
+  end
+
+  def catch_remainder
+    find_spare_seats
+    customers_allocated_spare_seats
+  end
+
+  def customers_allocated_spare_seats
+    @spare_tables.each do |table|
+      if table.seated_customers.size < 5
+        @array.each do |customer|
+          if table.seated_customers.size < 5
+            table.seated_customers << customer if !condition?(customer)
+          else
+            break
+          end
+        end
+      elsif table.seated_customers.size == 5
+        next
+      end
+    end
+  end
+
+  def find_spare_seats
+    @tables.each do |table|
+      if table.seated_customers.size < 5
+        @spare_tables << table
+      elsif table.seated_customers.size == 5
+        next    
+      end
+    end
+  end
+
+  def remainder
+    @customers - @all_seated_customers
+  end
+
+  def randomize_table
+    @tables[rand(@tables.size)]
   end
 
   def randomize_customer
-  	@temp = @customers[rand(@customers.size)] 
-  	if check? 
-  	  @temp = randomize_customer
-  	  @randoms << @temp
-  	  @customer = @temp		
-  	else
-  	  @randoms << @temp 
-  	  @customer = @temp
-  	end
+  	@customer = randomizer
+  end
+
+  def randomizer
+    @customers[rand(@customers.size)] 
   end
 
   def check?
-  	@randoms.include?(@temp) 
+  	@randoms.include?(@customer) 
   end
 
   def condition?(customer)
@@ -75,7 +120,7 @@ class Restaurant
   end
   
   def generate_customers
-  	25.times do
+  	rand(1..25).times do
   	  @customers << Customer.new 
   	end
   end
